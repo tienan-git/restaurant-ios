9c60393b18450f06c83ea24e618b40323ce77982
pakuseimi <paku.seimi@sparkworks.co.jp>
2018-09-23 17:27:10 +0900

    add setting

--- restaurant/CodeClass/Setting/Controller/SettingViewController.swift
+++ restaurant/CodeClass/Setting/Controller/SettingViewController.swift
@@ -9,22 +9,88 @@
 import UIKit
 
 class SettingViewController: UIViewController {
-
+    @IBOutlet weak var settingTableView: UITableView!
+    
+    fileprivate let itemDataSouce: [[(String)]] = [
+        [
+            (""),
+            ],
+        [
+            ("抽選応募履歴"),
+            ("フィードバック"),
+            ("利用規約"),
+            ("バージョン情報"),
+            ],
+        ]
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+        //self.view.backgroundColor = UIColor.viewBackgroundColor
+//        settingTableView.register(UINib(nibName: "SettingTableViewCell", bundle: nil), forCellReuseIdentifier: "SettingTableViewController")
+        
+        //settingTableView .register(SettingTableViewCell.classForCoder(), forCellReuseIdentifier: "SettingTableViewCell")
+        //self.settingTableView.ts_registerCellNib(SettingTableViewCell.self)
+       // settingTableView.dataSource = self
+       // settingTableView.delegate = self
+        self.settingTableView.tableFooterView = UIView()
 
         // Do any additional setup after loading the view.
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+// MARK: @protocol - UITableViewDelegate
+extension SettingViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 0 {
+            return 15
+        } else {
+            return 20
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0.00001
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+// MARK: @protocol - UITableViewDataSource
+//extension SettingViewController: UITableViewDataSource {
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return self.itemDataSouce.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        let rows = self.itemDataSouce[section]
+//        return rows.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        if indexPath.section == 0 {
+//            return 88.0
+//        } else {
+//            return 44.0
+//        }
+//    }
+
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        if indexPath.section == 0 {
+//            //自定义cell
+//           // let cell:SettingTableViewCell = settingTableView.dequeueReusableCell(withIdentifier: "SettingTableViewCell", for: indexPath) as! SettingTableViewCell
+//
+//          // let cell = settingTableView.dequeueReusableCell(withIdentifier: "SettingTableViewController", for: indexPath) as! SettingTableViewCell
+//
+//            return cell
+//        } else {
+//           //自定义cell
+//          // let cell:SettingTableViewCell = settingTableView.dequeueReusableCell(withIdentifier: "SettingTableViewCell", for: indexPath) as! SettingTableViewCell
+//           // let cell = settingTableView.dequeueReusableCell(withIdentifier: "SettingTableViewController", for: indexPath) as! SettingTableViewCell
+//            _ = self.itemDataSouce[indexPath.section][indexPath.row]
+//            return cell
+//        }
+//    }
+//}
