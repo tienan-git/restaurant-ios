1ecedf7d51618042ce1fd0d5ad26bc90b4514cf3
pakuseimi <paku.seimi@sparkworks.co.jp>
2018-09-23 18:44:40 +0900

    add フィードバック、利用規約、バージョンcontroller and xib

---
+++ restaurant/CodeClass/Setting/Controller/VersionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VersionViewController.swift
+//  restaurant
+//
+//  Created by パク・セイミ on 2018/09/23.
+//  Copyright © 2018年 劉鉄男. All rights reserved.
+//
+
+import UIKit
+
+class VersionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
