25e1936d03d929c33712d5506cefaea226f4b3e2
Tienan Liu <ryu.tetsuo@sparkworks.co.jp>
2018-09-23 17:40:43 +0900

    Model「Restaurant」作成

--- restaurant/Mode/Restaurant.swift
+++ restaurant/Mode/Restaurant.swift
@@ -12,53 +12,49 @@ import SwiftyJSON
 class Restaurant: Object {
     
     @objc dynamic var restaurantId: String = ""
-    @objc dynamic var restaurantName: String = ""
-    @objc dynamic var restaurantPhone: String = ""
-    @objc dynamic var restaurantOpenTime: String = ""
+    @objc dynamic var name: String = ""
+    @objc dynamic var address: String = ""
+    @objc dynamic var phoneNumber: String = ""
+    @objc dynamic var imageUrl: String = ""
     @objc dynamic var latitude: String = ""
     @objc dynamic var longitude: String = ""
-    @objc dynamic var restauranatAddress: String = ""
-    @objc dynamic var longitude: String = ""
-    @objc dynamic var longitude: String = ""
+    @objc dynamic var couponExistflag: String = ""
+    @objc dynamic var couponId: String = ""
+    @objc dynamic var couponName: String = ""
+    @objc dynamic var couponDetail: String = ""
+    @objc dynamic var couponPeriod: String = ""
     
     convenience init(json: JSON) {
         self.init()
-        
-        spotId = json["tour_spot_id"].stringValue
-        spotName = json["name1"].stringValue
-        spotType = json["spot_category_name"].stringValue
-        spotImageUrl = json["spots_images_url"].stringValue
-        spotLatitude = json["latitude"].stringValue
-        spotLongitude = json["longitude"].stringValue
-        spotService1 = json["チケット発行"].boolValue
-        spotService2 = json["チケット利用"].boolValue
-        spotService3 = json["LIQUID Pay 支払"].boolValue
-        spotService4 = json["LIQUID マネーチャージ"].boolValue
-        spotService5 = json["LIQUID ポイントチャージ"].boolValue
-        spotService6 = json["パスポート込ユーザー登録"].boolValue
-        spotService7 = json["既存ユーザーへの指紋登録"].boolValue
-        spotService8 = json["ユーザー照会"].boolValue
-        spotFlag = json["sp_flag"].stringValue
+        restaurantId = json["restaurantId"].stringValue
+        name = json["name"].stringValue
+        address = json["address"].stringValue
+        phoneNumber = json["phoneNumber"].stringValue
+        imageUrl = json["imageUrl"].stringValue
+        latitude = json["latitude"].stringValue
+        longitude = json["longitude"].stringValue
+        couponExistflag = json["couponExistflag"].stringValue
+        couponId = json["couponId"].stringValue
+        couponName = json["couponName"].stringValue
+        couponDetail = json["couponDetail"].stringValue
+        couponPeriod = json["couponPeriod"].stringValue
     }
     
     func convertIntoDictionary() -> Dictionary<String, Any> {
         let dic = [
-            "tour_spot_id": spotId,
-            "spotName": spotName,
-            "spotType": spotType,
-            "spotImageUrl": spotImageUrl,
-            "spotLatitude": spotLatitude,
-            "spotLongitude": spotLongitude,
-            "spotService1": spotService1,
-            "spotService2": spotService2,
-            "spotService3": spotService3,
-            "spotService4": spotService4,
-            "spotService5": spotService5,
-            "spotService6": spotService6,
-            "spotService7": spotService7,
-            "spotService8": spotService8,
-            "spotFlag": spotFlag
-            ] as [String : Any]
+            "restaurantId": restaurantId,
+            "name": name,
+            "address": address,
+            "phoneNumber": phoneNumber,
+            "imageUrl": imageUrl,
+            "latitude": latitude,
+            "longitude": longitude,
+            "couponExistflag": couponExistflag,
+            "couponId": couponId,
+            "couponName": couponName,
+            "couponDetail": couponDetail,
+            "couponPeriod": couponPeriod,
+        ] as [String : Any]
         return dic
     }
     
