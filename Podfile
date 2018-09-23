c7a8a901453584573efc6cc1a061bae62884748f
Tienan Liu <ryu.tetsuo@sparkworks.co.jp>
2018-09-23 12:01:28 +0900

    初期アップロード

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'restaurant' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'Alamofire-SwiftyJSON'
+  pod 'RealmSwift'
+  pod 'AlamofireImage'
+
+  # Pods for LotterApp
+
+end
---
+++ restaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  restaurant
+//
+//  Created by 劉鉄男 on 2018/9/23.
+//  Copyright © 2018年 劉鉄男. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
