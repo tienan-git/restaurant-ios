40e6c7c0fd96dfa6a64f92ede508b83293566a25
SOH RICHOH <soh.richoh@sparkworks.co.jp>
2018-10-07 00:51:27 +0900

    LotteryHistory added

---
+++ restaurant/Mode/LotteryHistory.swift
@@ -0,0 +1,40 @@
+//
+//  LotteryHistory.swift
+//  restaurant
+//
+//  Created by ソウ　リチョウ on 2018/10/07.
+//  Copyright © 2018年 劉鉄男. All rights reserved.
+//
+
+import RealmSwift
+import SwiftyJSON
+
+class LotteryHistory: Object {
+    
+    @objc dynamic var lotteryTitle: String = ""
+    @objc dynamic var lotteryDetail: String = ""
+    @objc dynamic var lotteryStatus: String = ""
+    @objc dynamic var applyDateTime: String = ""
+    
+    // JSONからObjectへ変換
+    convenience init(_ json: JSON) {
+        self.init()
+        
+        lotteryTitle = json["lotteryTitle"].stringValue
+        lotteryDetail = json["lotteryDetail"].stringValue
+        lotteryStatus = json["lotteryStatus"].stringValue
+        applyDateTime = json["applyDateTime"].stringValue
+    }
+    
+    // ObjectからDictionaryへ変換
+    func convertIntoDictionary() -> Dictionary<String, Any> {
+        let dic = [
+            "lotteryTitle": lotteryTitle,
+            "lotteryDetail": lotteryDetail,
+            "lotteryStatus": lotteryStatus,
+            "applyDateTime": applyDateTime
+            ] as [String : Any]
+        return dic
+    }
+    
+}
